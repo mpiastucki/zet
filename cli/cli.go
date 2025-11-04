3436eea4e4dbcc9beb6e1c85418895105ebd286c
mpiastucki <michael.piastucki@woven-planet.global>
2025-11-04 15:47:31 +0900

    update

--- cli/cli.go
+++ cli/cli.go
@@ -1,6 +1,28 @@
 package cli
 
+import (
+	"log"
+
+	"github.com/happymanju/zet/zet"
+)
+
+const commandPrompt string = "Enter Command >> "
+
 func Run(args []string) int {
+	fhm := zet.FileHashesMap{}
+	var err error
+
+	switch args[0] {
+	case "rebuild":
+		fhm, err = zet.BuildIndex()
+		if err != nil {
+			log.Printf("error rebuilding index: %v\n", err)
+			return 1
+		}
+	case "search":
+
+	case "update":
+	}
 	// make file hash table
 	// switch: user commands
 	// check save flag if saving data structs is needed
--- zet/zet.go
+++ zet/zet.go
@@ -1,7 +1,10 @@
 package zet
 
 import (
+	"bufio"
+	"encoding/gob"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"hash/fnv"
 	"io"
@@ -9,12 +12,77 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-type FileHashTable map[string]string
+var ErrNoMetadata error = errors.New("error: no metadata found")
 
-func (fht *FileHashTable) UpdateFile(fp string) error {
+type FileHashesMap struct {
+	FileHashes map[string]string
+	isChanged  bool
+}
+
+func (fhm *FileHashesMap) Update(p string) error {
+	f, err := os.Open(p)
+	if err != nil {
+		return fmt.Errorf("error in file hash update: could not open file: %w", err)
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return fmt.Errorf("error in FileHashesMap update: %w", err)
+	}
+
+	h, err := GetFNVHash(data)
+	if err != nil {
+		return fmt.Errorf("error in FileHashesMap update: %w", err)
+	}
 	//todo
+	hexHash := hex.EncodeToString(h)
+
+	v, ok := fhm.FileHashes[p]
+	if !ok {
+		fhm.FileHashes[p] = hexHash
+		fhm.isChanged = true
+	} else {
+		if v != hexHash {
+			fhm.FileHashes[p] = hexHash
+		}
+	}
+
+	return nil
+}
+
+func (fhm *FileHashesMap) Save() error {
+	if fhm.isChanged {
+		f, err := os.Create("zet.fhm.dat")
+		if err != nil {
+			return fmt.Errorf("error saving FileHashesMap: %w", err)
+		}
+		defer f.Close()
+
+		g := gob.NewEncoder(f)
+		g.Encode(fhm.FileHashes)
+		fhm.isChanged = false
+		return nil
+	}
+	return nil
+}
+
+func (fhm *FileHashesMap) Load() error {
+	f, err := os.Open("zet.fhm.dat")
+	if err != nil {
+		return fmt.Errorf("error loading FileHashesMap: %w", err)
+	}
+	defer f.Close()
+
+	g := gob.NewDecoder(f)
+	err = g.Decode(&fhm.FileHashes)
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error decoding FileHashesMap file: %w", err)
+	}
+
 	return nil
 }
 
@@ -28,13 +96,8 @@ func GetFNVHash(data []byte) ([]byte, error) {
 	return h.Sum(nil), nil
 }
 
-func ConvertHashToString(data []byte) string {
-	s := hex.EncodeToString(data)
-	return s
-}
-
-func BuildIndex() (FileHashTable, error) {
-	fht := FileHashTable{}
+func BuildIndex() (FileHashesMap, error) {
+	fht := FileHashesMap{}
 
 	var walkFunc fs.WalkDirFunc = func(p string, d fs.DirEntry, e error) error {
 		if d.IsDir() {
@@ -59,16 +122,59 @@ func BuildIndex() (FileHashTable, error) {
 			log.Printf("error generating hash from file %s data: %v\n", p, err)
 			return nil
 		}
-		stringedFileHash := ConvertHashToString(h)
+		stringedFileHash := hex.EncodeToString(h)
 
-		fht[p] = stringedFileHash
+		fht.FileHashes[p] = stringedFileHash
 
 		return nil
 	}
 
 	err := filepath.WalkDir(".", walkFunc)
 	if err != nil {
-		return nil, err
+		return fht, err
 	}
 	return fht, nil
+}
+
+func ParseMetadata(p string) (tags []string, err error) {
+	inTags := false
+
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file %s: %w", p, err)
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	sc.Scan()
+	firstLine := sc.Text()
+
+	if firstLine != "---" {
+		return nil, ErrNoMetadata
+	}
+
+	for sc.Scan() {
+		line := strings.TrimSpace(sc.Text())
+		if line == "---" {
+			break
+		}
+		var value string
+
+		if !inTags {
+			if strings.Contains(line, "tags:") {
+				inTags = true
+				continue
+			} else if strings.Contains(line, ":") {
+				inTags = false
+				continue
+			}
+		} else if inTags {
+			valueStart := strings.Index(line, "-")
+			value = line[valueStart+2:]
+			tags = append(tags, value)
+			continue
+		}
+	}
+
+	return tags, nil
 }
